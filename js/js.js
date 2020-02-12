83e9d3c2c6a111e6f1302ef6177f59bc2f96ace3
Alex Sebstian <42471288+AlexSebstian@users.noreply.github.com>
2020-02-12 11:29:31 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+
+
+function run(){
+
+	var V=document.getElementById("V").value;
+	if(V==10203)
+	{
+		alert('嘿，你居然发现了我的机关？太棒了！，我留给你一个链接，总之...当你看到这个消息时...我可能已经死了... --叶凉  ');
+		alert('https://alexsebstian.github.io/gaming/last_words.html');
+	}
+	else
+    	alert('提交成功');
+
+}
